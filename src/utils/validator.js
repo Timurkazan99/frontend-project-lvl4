ff9ae510b8185ab90f1e91e1fd92ff14acb74653
timurkazan99 <timurkazan99@gmail.com>
2022-07-09 19:20:12 +0300

    change email on username

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
 export const LoginSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
+    username: Yup.string().required('Required'),
     password: Yup.string().required('Required')
 });
