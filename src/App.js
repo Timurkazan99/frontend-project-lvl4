72e99b8565a6500cd5c38ce2a4ad303d67fa0a47
timurkazan99 <timurkazan99@gmail.com>
2022-07-11 12:59:15 +0300

    fix auth by token 2

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
         console.log(user.isAuth, token);
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    })
+    }, [])
 
     if(isLoading) {
       return (
