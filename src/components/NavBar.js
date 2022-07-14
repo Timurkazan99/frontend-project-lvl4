0351b4290c95296c77265fcdc5e7c6822e8119db
timurkazan99 <timurkazan99@gmail.com>
2022-07-15 00:57:54 +0300

    add logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import {Container, Navbar} from 'react-bootstrap';
-import {NavLink} from "react-router-dom";
-import {CHAT_ROUTE} from "../utils/const";
+import React, {useContext} from 'react';
+import {Button, Container, Navbar} from 'react-bootstrap';
+import {NavLink, useNavigate} from "react-router-dom";
+import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/const";
+import {Context} from "./ContextProvider";
 
 const NavBar = () => {
+    const {user} = useContext(Context);
+    const navigate = useNavigate()
+
+    const logout = () => {
+        user.setIsAuth(false);
+        user.setName('');
+        localStorage.clear();
+        navigate(LOGIN_ROUTE);
+    }
+
     return (
         <Navbar bg="dark" variant="dark">
-            <Container>
+            <Container className="d-flex justify-content-between">
                 <NavLink style={{color: "white"}} to={CHAT_ROUTE}>Slack chat</NavLink>
+                { user.isAuth ?
+                    <div>
+                        <span className="mx-3" style={{color: "white"}}>{user.name}</span>
+                        <Button variant={"primary"} onClick={logout}>Выйти</Button>
+                    </div>
+                    :
+                    null
+                }
             </Container>
         </Navbar>
     );
