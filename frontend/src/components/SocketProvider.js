d5c18e4c84ed74cc837cfc6b84734392c159760e
Timurkazan99 <Timurkazan99@gmail.com>
2022-07-17 20:02:04 +0300

    fix

--- frontend/src/components/SocketProvider.js
+++ frontend/src/components/SocketProvider.js
@@ -5,7 +5,7 @@ import useSocket from '../hooks/useSocket';
 export const SocketContext = createContext(null);
 
 function SocketProvider({ children }) {
-  const host = process.env.REACT_APP_HOST || 'http://localhost:5001'
+  const host = process.env.REACT_APP_HOST || 'http://localhost:5001';
   const socket = io(host, {
     transports: ['websocket'],
   });
