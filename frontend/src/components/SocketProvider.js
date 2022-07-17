788d93695e16afe96215950e17fb609c1808886c
Timurkazan99 <Timurkazan99@gmail.com>
2022-07-17 15:07:08 +0300

    fix

--- frontend/src/components/SocketProvider.js
+++ frontend/src/components/SocketProvider.js
@@ -5,7 +5,7 @@ import {io} from "socket.io-client";
 export const SocketContext = createContext(null);
 
 const SocketProvider = ({children}) => {
-    const socket = io('http://localhost:5000/', {
+    const socket = io('http://localhost:5001/', {
         transports: [ "websocket" ]
     });
 
