39c8f9c112343ee2865f00ae8e8e3628304b915a
timurkazan99 <timurkazan99@gmail.com>
2023-05-22 20:05:49 +0300

    fixes

--- frontend/src/components/IconBtn.jsx
+++ frontend/src/components/IconBtn.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
-import Icon from "./Icon";
-
-/*
-className="p-0 border-0 align-self-center"
-style={{ height: '24px', width: '24px', marginRight: '16px' }}
-onClick={() => dispatch(toList())}
-
-className="p-0 m-0 border-0"
-style={{ lineHeight: '0px', height: '24px', width: '24px' }}
-id="addChanel"
-onClick={createChannel}
-
-className="message-btn"
-style={{ lineHeight: '0px'}}
-type="submit"
-disabled={message === ''}
-
-
-*/
+import Icon from "./Icon.jsx";
 
 const IconBtn = (props) => {
     const {icon, text, ...btnProps} = props;
